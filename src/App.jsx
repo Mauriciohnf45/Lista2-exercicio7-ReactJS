8a9ce8ec9a373b396b0ad2f86b2e57897f533b79
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 22:33:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BoasVindas from './BoasVindas'; // Importando o novo componente
+
+function App() {
+  return (
+    <div>
+      <BoasVindas /> {/* Incluindo o componente BoasVindas */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BoasVindas.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BoasVindas() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Mundo do React</h1>
+      <h2>Aprenda a criar interfaces de forma simples e eficiente</h2>
+    </div>
+  );
+}
+
+export default BoasVindas;
